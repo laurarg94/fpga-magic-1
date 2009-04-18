08928bf3cd9af6f4a809a8cb32e6b4c7fc796538
leonardoaraujo.santos <leonardoaraujo.santos@f6f6bb9a-18be-11de-b69a-45dc1c5e4070>
2009-04-18 00:05:54 +0000

    Adding hex proms (Auto generated)

---
+++ trunk/hex proms/mcode.c
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------
+//
+//   WARNING - This is an auto-generated file.  Do not edit!  See
+//   http://www.homebrewcpu.com for details
+//
+//   Copyright 2001, 2002, 2003 Bill Buzbee - All Rights reserved (not that 
+//   anyone is likely to care...)
+//
+// -----------------------------------------------------------------------
+//
+#include "mcode.h"
+mcode_rec_t mcode_store[512] = {
+#include "prombits.h"
+};
