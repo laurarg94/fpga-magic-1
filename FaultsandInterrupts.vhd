636ddeef5ef2c1189961cad70c207efa7544b173
leonardoaraujo.santos <leonardoaraujo.santos@f6f6bb9a-18be-11de-b69a-45dc1c5e4070>
2009-05-10 13:42:52 +0000

    Some work in FaultsandInterrupts Adding PriorityEncoder

--- FaultsandInterrupts.vhd
+++ FaultsandInterrupts.vhd
@@ -28,39 +28,68 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity FaultsandInterrupts is
-    Port ( RESET : in  STD_LOGIC;
-           IRQ0 : in  STD_LOGIC;
-           IRQ1 : in  STD_LOGIC;
-           IRQ2 : in  STD_LOGIC;
-           IRQ3 : in  STD_LOGIC;
-           IRQ4 : in  STD_LOGIC;
-           IRQ5 : in  STD_LOGIC;
-           EL_FCODE : in  STD_LOGIC;
+    Port ( NEG_RESET : in  STD_LOGIC;
+           NEG_IRQ0 : in  STD_LOGIC;
+           NEG_IRQ1 : in  STD_LOGIC;
+           NEG_IRQ2 : in  STD_LOGIC;
+           NEG_IRQ3 : in  STD_LOGIC;
+           NEG_IRQ4 : in  STD_LOGIC;
+           NEG_IRQ5 : in  STD_LOGIC;
+           NEG_EL_FCODE : in  STD_LOGIC;
            MSWM : in  STD_LOGIC;
            PRIV : in  STD_LOGIC;
            MSWV : in  STD_LOGIC;
-           TRAPO : in  STD_LOGIC;
+           NEG_TRAPO : in  STD_LOGIC;
            CLKM : in  STD_LOGIC;
-           DMA_REQ : in  STD_LOGIC;
+           NEG_DMA_REQ : in  STD_LOGIC;
            MSWE : in  STD_LOGIC;
-           NP : in  STD_LOGIC;
-           NW : in  STD_LOGIC;
-           BKPT : in  STD_LOGIC;
-           SYSCALL : in  STD_LOGIC;
+           NEG_NP : in  STD_LOGIC;
+           NEG_NW : in  STD_LOGIC;
+           NEG_BKPT : in  STD_LOGIC;
+           NEG_SYSCALL : in  STD_LOGIC;
            IOCLK : in  STD_LOGIC;
-           NEXT0 : in  STD_LOGIC;
+           NEG_NEXT0 : in  STD_LOGIC;
            CLKS : in  STD_LOGIC;
-           CLR_TRAP : in  STD_LOGIC;
+           NEG_CLR_TRAP : in  STD_LOGIC;
            L : out  STD_LOGIC_VECTOR (0 to 15);
            RL_FAULT : out  STD_LOGIC;
            ENCODER : out  STD_LOGIC_VECTOR (0 to 3);
-           FAULT_PENDING : out  STD_LOGIC);
+           FAULT_PENDING : inout  STD_LOGIC);
 end FaultsandInterrupts;
 
 architecture Behavioral of FaultsandInterrupts is
+signal interrupt_register : std_logic_vector(0 to 7);
+signal interrupt_ff : std_logic_vector(0 to 5);
+signal output_priority_encoder : std_logic_vector(0 to 2);
+signal enable_input_u47 : std_logic;
+
+component PriorityEncoder
+	Port ( EnableInput : in  STD_LOGIC;
+           EncoderInputs : in  STD_LOGIC_VECTOR (7 downto 0);
+           EncoderOutputs : out  STD_LOGIC_VECTOR (2 downto 0);
+           EnableOutputs : out  STD_LOGIC;
+           GroupOutput : out  STD_LOGIC);
+end component;
 
 begin
-
+	-- Implement the Interrupt register U?
+	process (MSWE,interrupt_ff,NEG_DMA_REQ,NEG_RESET,CLKM)
+	begin
+		if NEG_RESET = '0' then
+			interrupt_register <= (others => '0');
+		elsif rising_edge(CLKM) then
+			interrupt_register <= MSWE & interrupt_ff & NEG_DMA_REQ;			
+		end if;
+	end process;
+	
+	-- Implement the logic of U47 74148 (8 to 3) priority encoders (http://en.wikipedia.org/wiki/Encoder#Single_bit_4_to_2_Encoder)
+	U47: PriorityEncoder port map (enable_input_u47, interrupt_register);	
+	
+	-- Implement the FF array U50 and U51
+	process (NEG_RESET,interrupt_ff,NEG_IRQ5,NEG_IRQ4,NEG_IRQ3,NEG_IRQ2,NEG_IRQ1,NEG_IRQ0)
+	begin
+		--if (NEG_RESET
+	end process;
 
 end Behavioral;
 
---
+++ PriorityEncoder.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 		Leonardo Araujo dos Santos
+-- 
+-- Create Date:    17:31:18 05/09/2009 
+-- Design Name: 
+-- Module Name:    PriorityEncoder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PriorityEncoder is
+    Port ( EnableInput : in  STD_LOGIC;
+           EncoderInputs : in  STD_LOGIC_VECTOR (7 downto 0);
+           EncoderOutputs : out  STD_LOGIC_VECTOR (2 downto 0);
+           EnableOutputs : out  STD_LOGIC;
+           GroupOutput : out  STD_LOGIC);	-- GroupOutput goes low on priority detect
+end PriorityEncoder;
+
+architecture Behavioral of PriorityEncoder is
+signal EncoderOutputs_Buffer : STD_LOGIC_VECTOR (2 downto 0);
+begin
+	-- Implementation of 8 input 74148 Priority encoder (I7 most priotity)
+	-- A priority is assigned to each input so that when
+	-- two or more inputs are simultaneously active, the input with the
+   -- highest priority is represented on the output, with input line I7 having
+   -- the highest priority.
+	
+	--A2 & A1 & A0
+	EncoderOutputs_Buffer <= "000" when (EncoderInputs(7) = '0') else		-- More priority (EncoderInputs(0)) NEG_I7
+									 "001" when (EncoderInputs(6) = '0') else
+									 "010" when (EncoderInputs(5) = '0') else
+									 "011" when (EncoderInputs(4) = '0') else
+									 "100" when (EncoderInputs(3) = '0') else
+									 "101" when (EncoderInputs(2) = '0') else
+									 "110" when (EncoderInputs(1) = '0') else	   
+									 "111";												   -- Less priority (EncoderInputs(7))
+	
+	EncoderOutputs <= EncoderOutputs_Buffer when (EnableInput = '0') else "111";
+	
+	-- GroupOutput is low if any input becomes low 
+	GroupOutput <= '0' when EncoderInputs(0) = '0' or EncoderInputs(1) = '0' or EncoderInputs(2) = '0' or EncoderInputs(3) = '0' or EncoderInputs(4) = '0' or EncoderInputs(5) = '0' or EncoderInputs(6) = '0' or EncoderInputs(7) = '0' else
+						'1';
+	
+	-- The EO is active-Low when all inputs are High
+	EnableOutputs <= '1' when EncoderInputs = "11111111" else '0';
+end Behavioral;
+
