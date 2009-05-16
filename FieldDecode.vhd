35586d8765edd9f43893faac0ca4a8923cdd3949
leonardoaraujo.santos <leonardoaraujo.santos@f6f6bb9a-18be-11de-b69a-45dc1c5e4070>
2009-05-16 23:32:33 +0000

    Work on FieldDecode still have to check things on TestBench and even the code

--- FieldDecode.vhd
+++ FieldDecode.vhd
@@ -31,7 +31,7 @@ entity FieldDecode is
     Port ( IR : in  STD_LOGIC_VECTOR (0 to 7);
            EL : in  STD_LOGIC_VECTOR (0 to 3);
            LATCH : in  STD_LOGIC_VECTOR (0 to 3);
-           RL_FPL : in  STD_LOGIC;
+           RL_FPL : out  STD_LOGIC;
            FP_L : in  STD_LOGIC_VECTOR (0 to 3);
            NEG_STOP_CLK : in  STD_LOGIC;
            RCOMMIT : in  STD_LOGIC;
@@ -59,7 +59,7 @@ entity FieldDecode is
            NEG_EL_TPC : out  STD_LOGIC;
            NEG_EL_SSP : out  STD_LOGIC;
            NEG_EL_MDR : out  STD_LOGIC;
-           NEG_DMA_ACK : out  STD_LOGIC;
+           NEG_DMA_ACK : inout  STD_LOGIC;
            RL_MSW : out  STD_LOGIC;
            NEG_EL_MAR : out  STD_LOGIC;
            NEG_EL_MSW : out  STD_LOGIC;
@@ -94,8 +94,11 @@ component EncoderDemux is
            Output : out  STD_LOGIC_VECTOR (7 downto 0)); -- O7,O6,O5,O4,O3,O2,O1,O0
 end component;
 
-signal EncoderAddress : std_logic_vector(0 to 2);
-signal EncoderOutputU33,EncoderOutputU32 : std_logic_vector(0 to 7);
+signal EncoderAddress,EncoderAddressU30U31,EncoderAddressU26U27 : std_logic_vector(0 to 2);
+signal EncoderOutputU33,EncoderOutputU32,EncoderOutputU30,EncoderOutputU31 : std_logic_vector(0 to 7);
+signal EncoderOutputU26,EncoderOutputU27,InverterTristateBufferOutput : std_logic_vector(0 to 7);
+signal U37Output,U34Output,U36Output : std_logic_vector(0 to 3);
+signal U35Output,U26G2AInput : std_logic;
 
 begin
 	NEG_ER_MDR <= ER(1);
@@ -124,6 +127,107 @@ begin
 	RL_IE <= EncoderOutputU32(5) nor NEG_CLKS;
 	NEG_DO_BRANCH <= EncoderOutputU32(4);
 	NEG_CLR_TRAP <= EncoderOutputU32(3) nor NEG_CLKS;
+	
+	-- Decode IR signals
+	-- Describe U37 74157 quad 2 to 1 Mux
+	process (EL,IR(6 to 7))
+	begin
+		-- A NAND port output is 0 if all inputs are high
+		if EL = "1111" then
+			-- Select is low ... select A Channel (4+IR(6..7))
+			U37Output <= "01" & IR(6) & IR(7); 
+		else
+			-- Select is high ... select B Channel (pass EL) (MAR,MSW,C,PC,DP,SP,A,B,MDR,PTB,SSP,TPC)
+			U37Output <= EL;
+		end if;
+	end process;
+	
+	-- Describe U34 74157 quad 2 to 1 Mux
+	process (U37Output,FP_L,NEG_STOP_CLK,NEG_DMA_ACK)
+	begin
+		if NEG_STOP_CLK = '1' and NEG_DMA_ACK = '1' then
+			-- Select is low ... select A Channel
+			U34Output <= U37Output;
+		else
+			-- Select is high ... select B Channel
+			U34Output <= FP_L;
+		end if;
+	end process;
+	
+	-- Describe U35 7485 Comparator
+	process (FP_L,U34Output)
+	begin
+		if FP_L = U34Output then
+			U35Output <= '1';
+		else
+			U35Output <= '0';
+		end if;
+	end process;
+	
+	-- RL_FPL Output description
+	RL_FPL <= (NEG_CLKS and U35Output) or (FP_L(0) and FP_L(3) and (FP_L(1) nor FP_L(2)));
+	
+	-- Describe U30 and U31 encoder/demultiplexer behavior handling the MISC signals
+	-- the U34Output(0) input will enable/disable U30 or U31
+	EncoderAddressU30U31 <= U34Output(1 to 3);
+	U30: EncoderDemux port map(U34Output(0),'0','0',EncoderAddressU30U31,EncoderOutputU30);
+	U31: EncoderDemux port map('1',U34Output(0),'0',EncoderAddressU30U31,EncoderOutputU31);	
+	
+	NEG_EL_MAR <= EncoderOutputU31(7);
+	NEG_EL_MSW <= EncoderOutputU31(6);
+	NEG_EL_C   <= EncoderOutputU31(5);
+	NEG_EL_PC  <= EncoderOutputU31(4);
+	NEG_EL_DP  <= EncoderOutputU31(3);
+	NEG_EL_SP  <= EncoderOutputU31(2);
+	NEG_EL_A   <= EncoderOutputU31(1);
+	NEG_EL_B   <= EncoderOutputU31(0);
+	
+	NEG_EL_MDR <= EncoderOutputU30(7);
+	NEG_EL_SSP <= EncoderOutputU30(5);
+	NEG_EL_TPC <= EncoderOutputU30(4);
+	NEG_EL_FCODE <= EncoderOutputU30(3);
+	
+	-- Use IR/Latch bits to select some registers
+	-- Describe U36 74157 quad 2 to 1 Mux
+	process(IR(5 to 7),LATCH)
+	begin
+		-- A NAND port output is 0 if all inputs are high
+		if LATCH = "1111" then
+			-- Select is low ... select A Channel "0 & IR(5..7)"
+			U36Output <= '0' & IR(5 to 7);
+		else
+			-- Select is high ... select B Channel "LATCH"
+			U36Output <= LATCH;
+		end if;
+	end process;
+	
+	-- Describe U26 and U27 encoder/demultiplexer behavior handling the IR and LATCH signals
+	-- the CLKS and FAULT_PENDING input will enable/disable U26 or U27
+	EncoderAddressU26U27 <= U34Output(1 to 3);
+	U26G2AInput <= not U36Output(0);
+	U26: EncoderDemux port map(CLKS,FAULT_PENDING,U26G2AInput,EncoderAddressU26U27,EncoderOutputU26);
+	U27: EncoderDemux port map(CLKS,FAULT_PENDING,U36Output(0),EncoderAddressU26U27,EncoderOutputU27);	
+	
+	-- U29 TristateBuffer 74F240 will just invert signals in this case.
+	InverterTristateBufferOutput <= not EncoderOutputU27;
+	
+	RL_C    <= InverterTristateBufferOutput(2);
+	RL_PC   <= InverterTristateBufferOutput(3);
+	RL_DP   <= InverterTristateBufferOutput(4);
+	RL_SP   <= InverterTristateBufferOutput(5);
+	RL_A_LO <= InverterTristateBufferOutput(6);
+	RL_B_LO <= InverterTristateBufferOutput(7);
+	
+	RL_A_HI <= InverterTristateBufferOutput(6) and LATCH_SZ;
+	RL_B_HI <= InverterTristateBufferOutput(7) and LATCH_SZ;
+	
+	RL_PTB <= not EncoderOutputU26(6);
+	RL_MDR <= not EncoderOutputU26(7);
+	
+	RL_SSP <= InverterTristateBufferOutput(5) and (not MSWM);
+	
+	RL_MSW <= (not FAULT_PENDING) and (InverterTristateBufferOutput(1) or (NEG_CLKS nor EncoderOutputU33(1)));
+	
 
 end Behavioral;
 
